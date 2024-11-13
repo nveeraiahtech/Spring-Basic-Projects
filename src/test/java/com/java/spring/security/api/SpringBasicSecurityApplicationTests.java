02967966b6e7f9f17fe39d8d425de9d96cbb9ea2
veera <nveeraiahtech@gmail.com>
2024-11-12 21:57:49 -0600

    First Commit

---
+++ src/test/java/com/java/spring/security/api/SpringBasicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.spring.security.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
